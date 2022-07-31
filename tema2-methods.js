61f7d71bbf998c373def8305d9471bd38d95a576
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-07-31 16:52:07 +0200

    Add Exercises of Methods

--- tema2-methods.js
+++ tema2-methods.js
@@ -1,7 +1,88 @@
+//               METHODS
 // Ejercicio 1:
-const myName = "Aldana";
-function lettersOfMyName(){
-    return "My name has " + myName.length + " letters";
-}
-const result = lettersOfMyName();
-console.log(result);
+// const myName = "Aldana";
+// function lettersOfMyName(){
+//     return "My name has " + myName.length + " letters";
+// }
+// const result = lettersOfMyName();
+// console.log(result);
+
+// const myName = "Aldana";
+// const lettersOfMyName = () => {
+//     return "My name has " + myName.length + " letters";
+// }
+// const result = lettersOfMyName();
+// console.log(result);
+
+// Ejercicio 2:
+// const myFullName = "Aldana Arcidiacono";
+// const fullNameStarts = () => {
+//     return "My last name starts on positon " + myFullName.lastIndexOf("A");
+// }
+// const result = fullNameStarts();
+// console.log(result);
+
+// Ejercicio 3:
+// const myFullName = "Aldana Arcidiacono";
+// const onlyMyFirstName = () => {
+//     return "My name is " + myFullName.slice(0,6);
+// }
+// const result = onlyMyFirstName();
+// console.log(result);
+
+// Ejercicio 4:
+// const myFullName = "Aldana Arcidiacono";
+// const onlyMySurname = () => {
+//     return myFullName.slice(7,18);
+// }
+// const result = onlyMySurname();
+// console.log(result);
+
+// Ejercicio 4.1:
+// const mySurname = result;
+// console.log(myFullName + ", " + mySurname);
+
+// Ejercicio 5:
+// const myFullName = "Aldana Arcidiacono";
+// const onlyMySurname = () => {
+//     return myFullName.slice(7,18);
+// }
+// const result = onlyMySurname();
+// const mySurname = result;
+// console.log("Hello Ms. " + mySurname);
+
+// Ejercicio 6:
+// const myFullName = "Aldana Arcidiacono";
+
+// const onlyMySurname = () => {
+//     return myFullName.slice(7,18);
+// }
+// const result = onlyMySurname();
+// const mySurname = result;
+
+// const surnameToUpperCase = () => {
+//     return result.toUpperCase();
+// }
+// const newResult = surnameToUpperCase();
+// const myUpperCaseSurname = newResult;
+
+// console.log("my last name is " + myUpperCaseSurname);
+
+// Ejercicio 7:
+// const myFullName = "Aldana Arcidiacono";
+
+// const onlyMyFirstName = () => {
+//     return myFullName.slice(0,6);
+// }
+
+// const result = onlyMyFirstName();
+
+// let adjective = result + " is awesome";
+// console.log(adjective);
+
+// Ejercicio 8:
+// const myFullName = "Aldana Arcidiacono";
+// console.log("Mis iniciales son: " + myFullName.substring(0,1) + ". " + myFullName.substring(7,8) + ".");
+
+//               ARRAYS
+// Ejercicio 1:
