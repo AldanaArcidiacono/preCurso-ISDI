cbc9940988c96603ae9b453981ef5b8f1e28e40c
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-08-11 12:28:41 +0200

    Scales fix

--- Proyectos/Airlines-tema3.js
+++ Proyectos/Airlines-tema3.js
@@ -1,5 +1,5 @@
 // Estos vuelos estarán declarados de manera global:
-const listOfFlights = [
+const flights = [
     { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
     { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
     { id: 02, to: "Paris", from: "Barcelona", cost: 210, scale: false },
@@ -24,22 +24,27 @@ const getUserName = () => {
 getUserName();
 
 // El usuario visualizará todos los vuelos disponibles de una forma amigable: 
-// TODOS los vuelos? Re largo sería si lo tuviera que poner así con c/u, como puede ir?
+// Podrá ver cuantos vuelos efectúan escalas.
+const scales = () => {
+    //let hasScale = "";
+    flights.forEach((flights) => {
+        if (flights.scale) {
+            flights.scale = "realiza escalas."
+        } else {
+            flights.scale = "no realiza ninguna escala."
+        }
+        console.log(`El vuelo con origen: ${flights.from}, y destino: ${flights.to}, tiene un coste de €${flights.cost} y ${flights.scale}`)
+    });
+}
+scales();
+
 // El vuelo con origen: Barcelona, y destino: Madrid tiene un coste de 90€ y no realiza ninguna escala
-const availableFlights = confirm(`El vuelo con origen: ${listOfFlights[5].from}, y destino: ${listOfFlights[5].to}, tiene un coste de €${listOfFlights[5].cost} y escalas ${listOfFlights[5].scale}`);
+const availableFlights = confirm(`El vuelo con origen: ${flights[5].from}, y destino: ${flights[5].to}, tiene un coste de €${flights[5].cost} y escalas ${flights[5].scale}`);
 
 // El usuario verá el coste medio de los vuelos.
 
-// Podrá ver cuantos vuelos efectúan escalas.
-// const scales = () => {
-//     if (!listOfFlights.scale)
-//     {
-
-//     }
-// }
-
 // Y, sabiendo que los ultimos 5 vuelos (los últimos 5 ID's) son los últimos del día, muestra al usuario sus destinos.
 const lastDestinations = alert(`Los últimos vuelos programados para hoy son hacia los destinos:
-${listOfFlights[9].to}, ${listOfFlights[8].to}, ${listOfFlights[7].to}, ${listOfFlights[6].to} y ${listOfFlights[5].to}`)
+${flights[9].to}, ${flights[8].to}, ${flights[7].to}, ${flights[6].to} y ${flights[5].to}`)
 
 // This is a example of array of objects... each position of array contains one object...
