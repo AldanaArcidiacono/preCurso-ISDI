222e9e9151b609943b52d763f078e8352b06ccfb
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-09-01 14:05:07 +0200

    Add questions

--- Proyectos/Pasapalabra-tema6.js
+++ Proyectos/Pasapalabra-tema6.js
@@ -42,26 +42,28 @@ const greetingAndGetName = () => {
     return userName;
 }
 
-// Esto debería aprecer al final. Acomodar puntos una vez este definido.
-const storingPlayerNames = (userName, currentPlayerScore) => {
-    let playerNames = [
-        {name: "Donna", score: `${Math.floor(Math.random() * (84 - 1) + 1)}`},
-        {name: "Clara", score: `${Math.floor(Math.random() * (84 - 1) + 1)}`},
-        {name: "Amy", score: `${Math.floor(Math.random() * (84 - 1) + 1)}`},
-        {name: `${userName}`, score: `${currentPlayerScore}`}
-    ];
-    playerNames.sort((a , b) => b.score - a.score);
-    alert(`Este es el ranking de nuestros usuarios:\n${playerNames[0].name}: ${playerNames[0].score} puntos\n${playerNames[1].name}: ${playerNames[1].score} puntos\n${playerNames[2].name}: ${playerNames[2].score} puntos\n${playerNames[3].name}: ${playerNames[3].score} puntos`);
-}
 
-const playAgain = (userName) => {
-    const newGame = confirm("Haz click en 'aceptar' si deseas jugar de nuevo🎲🎱. De lo contrario, haz click en 'cancelar'");
-    if (newGame){
-        alphabeticalGame();
-    } else {
-        alert(`Gracias por jugar a Pasapalabra Game! ${userName}!🤗🎡📚 Nos vemos la próxima!👋🏻`);
-    }
-}
+
+// Esto debería aprecer al final. Acomodar puntos una vez este definido.
+// const storingPlayerNames = (userName, currentPlayerScore) => {
+//     let playerNames = [
+//         {name: "Donna", score: `${Math.floor(Math.random() * (84 - 1) + 1)}`},
+//         {name: "Clara", score: `${Math.floor(Math.random() * (84 - 1) + 1)}`},
+//         {name: "Amy", score: `${Math.floor(Math.random() * (84 - 1) + 1)}`},
+//         {name: `${userName}`, score: `${currentPlayerScore}`}
+//     ];
+//     playerNames.sort((a , b) => b.score - a.score);
+//     alert(`Este es el ranking de nuestros usuarios:\n${playerNames[0].name}: ${playerNames[0].score} puntos\n${playerNames[1].name}: ${playerNames[1].score} puntos\n${playerNames[2].name}: ${playerNames[2].score} puntos\n${playerNames[3].name}: ${playerNames[3].score} puntos`);
+// }
+
+// const playAgain = (userName) => {
+//     const newGame = confirm("Haz click en 'aceptar' si deseas jugar de nuevo🎲🎱. De lo contrario, haz click en 'cancelar'");
+//     if (newGame){
+//         alphabeticalGame();
+//     } else {
+//         alert(`Gracias por jugar a Pasapalabra Game! ${userName}!🤗🎡📚 Nos vemos la próxima!👋🏻`);
+//     }
+// }
 
 // Main function:
 const alphabeticalGame = () => {};
